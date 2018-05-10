83bea4d68d5f4b65ab84cba26fc6688bfda3d3a8
STEdirisuriya <newsapp.acicts@gmail.com>
2018-05-10 20:41:52 +0530

    leaderbiards updated

--- main.js
+++ main.js
@@ -22,17 +22,25 @@ new Vue({
       { name: 'Susan', score: 21, grade: 9, userId:'asd123' }
     ]*/
     users: [
-      { name: 'H. Jayasahan Hansana', score: 58, grade: 10, userId:'100135885',tasks:'4', favourite:'Design'},
+      { name: 'H. Jayasahan Hansana', score: 74, grade: 10, userId:'100135885',tasks:'5', favourite:'Design'},
       { name: 'Janidu Arinda Fernando', score: 30, grade: 9, userId:'090741612',tasks:'2', favourite:'Design'},
       { name: 'Heliya Nithira Suriya Bandara', score: 15, grade: 11, userId:'111101615',tasks:'1', favourite:'Design'},
-      { name: 'W.M. Janiru Damsath Bandara', score: 16, grade: 7, userId:'070839299',tasks:'1', favourite:'Code'},
+      { name: 'W.M. Janiru Damsath Bandara', score: 32, grade: 7, userId:'070839299',tasks:'2', favourite:'Code'},
       { name: 'T. Nethula Roshane Peiris', score: 29, grade: 7, userId:'070838256',tasks:'2', favourite:'Design'},
       { name: 'Thulina Dinsara Perera', score: 23, grade: 9, userId:'091030109',tasks:'2', favourite:'Design'},
       { name: 'H. Sithira Ransara Seneviratne', score: 12, grade: 8, userId:'080223301',tasks:'1', favourite:'Design'},
       { name: 'Senel Demintha', score: 41, grade: 11, userId:'110214810',tasks:'1', favourite:'Design'},
       { name: 'Banuja Nethusara Dehigaspage', score: 18, grade: 8, userId:'080830456',tasks:'1', favourite:'Design'},
-      { name: 'Inuka Sanudaya Batawala', score: 15, grade: 7, userId:'070516651',tasks:'1', favourite:'Code'},
-      { name: 'J.B. Bihandu Semal', score: 14, grade: 9, userId:'091008008',tasks:'1', favourite:'Code'}
+      { name: 'Inuka Sanudaya Batawala', score: 30, grade: 7, userId:'070516651',tasks:'2', favourite:'Code/Design'},
+      { name: 'J.B. Bihandu Semal', score: 29, grade: 9, userId:'091008008',tasks:'2', favourite:'Code/Design'},
+      { name: 'J.A. Davindu Reshan', score: 41, grade: 11, userId:'111147704',tasks:'3', favourite:'Design'},
+      { name: 'Chamod Nilushanka Perera', score: 32, grade: 11, userId:'111141143',tasks:'2', favourite:'Design'},
+      { name: 'Nayantha Nethsara Senavithilaka', score: 32, grade: 10, userId:'101039302',tasks:'2', favourite:'Design'},
+      { name: 'E.A. Chamuthu Megha Malhara Edirisooriya', score: 13, grade: 10, userId:'100148900',tasks:'1', favourite:'Code'},
+      { name: 'T. Didula Dinoj Pieris', score: 15, grade: 9, userId:'090125136',tasks:'1', favourite:'Design'},
+      { name: 'R.M. Tenura Geenath', score: 15, grade: 8, userId:'080835791',tasks:'1', favourite:'Design'},
+      { name: 'M.M.A. Pahan Samudika Abhayawardhane', score: 19, grade: 11, userId:'110418733',tasks:'1', favourite:'Design'},
+      { name: 'K.A. Samiru Thisaraka De Silva', score: 17, grade: 11, userId:'110842334',tasks:'1', favourite:'Design'}
     ]
   },
 
